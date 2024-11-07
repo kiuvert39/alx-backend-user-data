d75a76b9f0002dd34a2d2491ad23ee5398530c3b
kliuvert <forrexplug@gmail.com>
2024-11-07 18:30:04 -0500

    regex

---
+++ filtered_logger.py
@@ -0,0 +1,10 @@
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+    for field in fields:
+        message = re.sub(f'{field}=(.*?){separator}',f'{field}={redaction}{separator}', message)
+    
+    return message
