f8fe85a5f1a98055372bd7e0a9b22a0fa0d95039
kliuvert <forrexplug@gmail.com>
2024-11-07 15:43:01 +0000

    log files

--- filtered_logger.py
+++ filtered_logger.py
@@ -5,6 +5,7 @@ import re
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
 
     for field in fields:
-        message = re.sub(f'{field}=(.*?){separator}',f'{field}={redaction}{separator}', message)
+        message = re.sub(f'{field}=(.*?){separator}', 
+f'{field}={redaction}{separator}', message)
     
     return message
