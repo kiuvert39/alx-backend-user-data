44d59189403efaa09118320246575aab1b05eee1
kliuvert <forrexplug@gmail.com>
2024-11-07 20:12:15 -0500

    trying

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,8 @@
 from typing import List
 import re
 
-
 def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-
+                  separator: str) -> str:
     for field in fields:
         message = re.sub(f'{field}=(.*?){separator}',
                          f'{field}={redaction}{separator}', message)
